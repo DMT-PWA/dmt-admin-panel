12f949d29311388f23d8851ef503ca0dcc919f27
akushnarenko <KushnarenkoAD@gmail.com>
2025-08-31 23:37:51 +0300

    Reapply "update notifications"
    This reverts commit 8a4ac43b3ea10129663dd210e752b0c72674f1e7.

--- src/widgets/NotificationForm/lib/notificationForm.thunk.ts
+++ src/widgets/NotificationForm/lib/notificationForm.thunk.ts
@@ -1,6 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { apiInstance } from "src/shared/api/base";
-import { NotificationSettings } from "src/shared/types/notification.types";
+import {
+  NotificationMessage,
+  NotificationSettings,
+  NotificationTime,
+} from "src/shared/types/notification.types";
 
 export const getAllPwa = createAsyncThunk<NotificationSettings["pwas"]>(
   "table/getAllPwa",
@@ -8,3 +12,24 @@ export const getAllPwa = createAsyncThunk<NotificationSettings["pwas"]>(
     return await apiInstance.get("/pwa");
   }
 );
+
+type CreateNotificationPayload = {
+  messages: NotificationMessage[];
+  adminId: string;
+  appIds: Array<string>;
+  title: NotificationSettings["title"];
+  defaultLanguage: NotificationSettings["defaultLanguage"];
+  category: string;
+  schedules: {
+    days: NotificationTime["days"];
+    isRecurring: NotificationTime["isRecurring"];
+    time: string;
+  }[];
+};
+
+export const createNotification = createAsyncThunk<
+  unknown,
+  CreateNotificationPayload
+>("notification/createNotification", async (payload) => {
+  return await apiInstance.post("/notifications", payload);
+});
--- src/widgets/NotificationTable/ui/PushNotificationTable.tsx
+++ src/widgets/NotificationTable/ui/PushNotificationTable.tsx
@@ -7,23 +7,20 @@ import {
   PaginationState,
   useReactTable,
 } from "@tanstack/react-table";
-import { FC, useCallback, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Title } from "src/shared/ui/title";
-import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "src/shared/lib/store";
-import { setAppId, createRenderService } from "src/entities/pwa_create";
 import clsx from "clsx";
-import { ClonePwaPayload, RowDefaultType } from "../lib/types";
+import { RowDefaultType } from "../lib/types";
 import { createColumnHelper } from "@tanstack/react-table";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import copy_icon from "src/shared/assets/icons/copy_icon.png";
-import link_icon from "src/shared/assets/icons/link_icon.png";
 import options_icon from "src/shared/assets/icons/options_icon.png";
 import trash from "src/shared/assets/icons/trash_icon_orange.png";
 import pencil from "src/shared/assets/icons/pencil.png";
-import { ButtonDefault } from "src/shared/ui/button";
 import { getAllNotifications } from "src/entities/notification/model/notification.thunk";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/16/solid";
 
 export const PushNotificationTable: FC = () => {
   const [notifications, setNotifications] = useState<Array<RowDefaultType>>([]);
@@ -32,6 +29,8 @@ export const PushNotificationTable: FC = () => {
     pageSize: 10,
   });
   const dispatch = useAppDispatch();
+
+  const onCopyHandler = (value: string) => navigator.clipboard.writeText(value);
 
   const navigate = useNavigate();
 
@@ -47,19 +46,86 @@ export const PushNotificationTable: FC = () => {
     fetchData();
   }, [dispatch]);
 
-  const columnHelper = createColumnHelper<RowDefaultType>();
+  const columnHelper = createColumnHelper<
+    RowDefaultType & { actions?: string }
+  >();
   const columns = [
     columnHelper.accessor("adminId", {
       header: "PWA",
+      cell: (adminId) => (
+        <>
+          <span>{adminId.getValue()}</span>
+          <button onClick={() => onCopyHandler(adminId.getValue())}>
+            <img src={copy_icon} style={{ height: "12px", width: "12px" }} />
+          </button>
+        </>
+      ),
+      size: 100,
     }),
     columnHelper.accessor("status", {
       header: "Status",
+      size: 100,
     }),
     columnHelper.accessor("language", {
       header: "Default language",
+      size: 100,
     }),
     columnHelper.accessor("title", {
       header: "Name",
+      cell: (adminId) => (
+        <>
+          <span>{adminId.getValue()}</span>
+          <button onClick={() => onCopyHandler(adminId.getValue())}>
+            <img src={copy_icon} style={{ height: "12px", width: "12px" }} />
+          </button>
+        </>
+      ),
+      size: 100,
+    }),
+    columnHelper.accessor("actions", {
+      header: "",
+      cell: (cell) => (
+        <div className="min-w-23 flex justify-around">
+          <Menu>
+            <MenuButton>
+              <img
+                src={options_icon}
+                style={{ height: "4px", width: "16px" }}
+              />
+            </MenuButton>
+            <MenuItems
+              transition
+              anchor="bottom end"
+              className="origin-top-right rounded-xl border border-none bg-white pb-3.25 pt-2.25 pl-4.5 pr-5  transition duration-100 ease-out focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+            >
+              <MenuItem>
+                <button className="group flex w-full items-center gap-2 rounded-lg text__default text-view-7 mb-4">
+                  <img
+                    src={copy_icon}
+                    style={{ height: "14px", width: "14px" }}
+                  />
+                  Клонировать
+                </button>
+              </MenuItem>
+
+              <div className="my-1 h-px bg-white/5" />
+              <MenuItem>
+                <button className="group flex w-full items-center gap-2 rounded-lg text__default text-view-7">
+                  <img
+                    src={trash}
+                    style={{
+                      height: "16px",
+                      width: "14px",
+                    }}
+                  />
+                  Удалить
+                </button>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
+      ),
+      size: 500,
     }),
   ];
 
@@ -106,13 +172,18 @@ export const PushNotificationTable: FC = () => {
               {table.getRowModel().rows.map((row) => {
                 return (
                   <tr key={row.id} className="bg-white">
-                    {row.getVisibleCells().map((cell) => {
+                    {row.getVisibleCells().map((cell, ind) => {
                       return (
                         <td
                           key={cell.id}
                           className="px-6 py-4 text-view-1 text-black-1 border-b border-b-gray-7"
                         >
-                          <div className="flex gap-1.25 items-center">
+                          <div
+                            className={clsx("flex gap-1.25 items-center", {
+                              "justify-end":
+                                ind === row.getVisibleCells().length - 1,
+                            })}
+                          >
                             {flexRender(
                               cell.column.columnDef.cell,
                               cell.getContext()
@@ -127,6 +198,84 @@ export const PushNotificationTable: FC = () => {
             </tbody>
           </table>
         )}
+      </div>
+      <div className="mt-6.5 flex justify-center items-center">
+        {table.getCanPreviousPage() && (
+          <button
+            className={clsx(
+              "flex items-center text-view-11 p-2",
+              !table.getCanPreviousPage() ? "text-[#717171]" : "text-[#FF5532]"
+            )}
+            onClick={() => table.previousPage()}
+          >
+            <ChevronLeftIcon
+              className={clsx(
+                "size-6",
+                !table.getCanPreviousPage()
+                  ? "text-[#717171]"
+                  : "text-[#FF5532]"
+              )}
+            />
+            Предыдущая
+          </button>
+        )}
+        {(() => {
+          const pageCount = table.getPageCount();
+          const currentPage = table.getState().pagination.pageIndex;
+          const visiblePages = [];
+
+          visiblePages.push(1);
+
+          const rangeStart = Math.max(2, currentPage - 1);
+          const rangeEnd = Math.min(pageCount - 1, currentPage + 3);
+
+          if (rangeStart > 2) visiblePages.push("...");
+          for (let i = rangeStart; i <= rangeEnd; i++) {
+            visiblePages.push(i);
+          }
+          if (rangeEnd < pageCount - 1) visiblePages.push("...");
+
+          if (pageCount > 1) visiblePages.push(pageCount);
+
+          return (
+            <div className="flex items-center gap-1">
+              {visiblePages.map((page, index) =>
+                page === "..." ? (
+                  <span key={`ellipsis-${index}`} className="px-2">
+                    ...
+                  </span>
+                ) : (
+                  <button
+                    key={page}
+                    className={`w-8 h-8 rounded-md ${
+                      currentPage + 1 === page ? "bg-[#FF9079] text-white" : ""
+                    }`}
+                    onClick={() => table.setPageIndex((page as number) - 1)}
+                    disabled={currentPage + 1 === page}
+                  >
+                    {page}
+                  </button>
+                )
+              )}
+            </div>
+          );
+        })()}
+        <button
+          className={clsx(
+            "flex items-center text-view-11 p-2",
+            !table.getCanNextPage() ? "text-[#717171]" : "text-[#FF5532]"
+          )}
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Следующая
+          <ChevronRightIcon
+            className={clsx(
+              "size-6",
+              !table.getCanNextPage() ? "text-[#717171]" : "text-[#FF5532]"
+            )}
+          />
+        </button>
       </div>
     </>
   );
